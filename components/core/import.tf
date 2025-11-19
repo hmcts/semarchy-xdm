39db604d6baa2797ca52ac85ca2afada99197e46
Alex Bance <Alex.Bance@justice.gov.uk>
2025-11-19 13:34:01 +0000

    fix: remove imports

--- components/core/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  id = "/subscriptions/b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb/resourceGroups/semarchy-xdm-core-rg/providers/Microsoft.KeyVault/vaults/csds-keyvault-sbox/objectId/173fde48-bd6b-44f1-bb63-6fe6c90424ca"
-  to = module.key_vault.azurerm_key_vault_access_policy.creator_access_policy
-}
