c6eefd44e2b0dc28a42ea2b443c3efc89cf41e53
Alex Bance <Alex.Bance@justice.gov.uk>
2025-12-11 08:56:09 +0000

    feat: switch to main ref

--- components/container-app/main.tf
+++ components/container-app/main.tf
@@ -24,7 +24,7 @@ data "azurerm_log_analytics_workspace" "main" {
 }
 
 module "container_app" {
-  source = "github.com/hmcts/terraform-module-azure-container-app?ref=feat/workload-profiles"
+  source = "github.com/hmcts/terraform-module-azure-container-app?ref=main"
 
   providers = {
     azurerm     = azurerm
