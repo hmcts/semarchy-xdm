1288ff5114476e31ce2236252a37b1f20113ed6e
Alex Bance <Alex.Bance@justice.gov.uk>
2025-12-15 11:50:08 +0000

    fix: correct vnet link name

--- components/database/main.tf
+++ components/database/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_private_dns_zone_virtual_network_link" "postgresql" {
   provider              = azurerm.dts_intsvc
-  name                  = "postgresql-vnet-link-${var.env}"
+  name                  = "csds-${var.env}"
   resource_group_name   = local.dts_dns_resource_group
   private_dns_zone_name = data.azurerm_private_dns_zone.postgresql.name
   virtual_network_id    = data.azurerm_virtual_network.csds.id
